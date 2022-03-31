5bbcac6dc6ad616af312741e75a8e776fcada1c5
Shilpa <skcodes29@gmail.com>
2022-03-31 17:36:39 +0530

    unnecessary details removed from post page

--- src/components/home/posts-list-results.js
+++ src/components/home/posts-list-results.js
@@ -23,38 +23,6 @@ export const PostsListResults = ({ posts, ...rest }) => {
   const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(0);
 
-  /*const handleSelectAll = (event) => {
-    let newSelectedpostIds;
-
-    if (event.target.checked) {
-      newSelectedpostIds = posts.map((post) => post.id);
-    } else {
-      newSelectedpostIds = [];
-    }
-
-    setSelectedpostIds(newSelectedpostIds);
-  };
-
-  const handleSelectOne = (event, id) => {
-    const selectedIndex = selectedpostIds.indexOf(id);
-    let newSelectedpostIds = [];
-
-    if (selectedIndex === -1) {
-      newSelectedpostIds = newSelectedpostIds.concat(selectedpostIds, id);
-    } else if (selectedIndex === 0) {
-      newSelectedpostIds = newSelectedpostIds.concat(selectedpostIds.slice(1));
-    } else if (selectedIndex === selectedpostIds.length - 1) {
-      newSelectedpostIds = newSelectedpostIds.concat(selectedpostIds.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelectedpostIds = newSelectedpostIds.concat(
-        selectedpostIds.slice(0, selectedIndex),
-        selectedpostIds.slice(selectedIndex + 1)
-      );
-    }
-
-    setSelectedpostIds(newSelectedpostIds);
-  };*/
-
   const handleLimitChange = (event) => {
     setLimit(event.target.value);
   };
@@ -109,30 +77,3 @@ export const PostsListResults = ({ posts, ...rest }) => {
 PostsListResults.propTypes = {
   posts: PropTypes.array.isRequired
 };
-/*<Checkbox
-                    checked={selectedpostIds.length === posts.length}
-                    color="primary"
-                    indeterminate={
-                      selectedpostIds.length > 0
-                      && selectedpostIds.length < posts.length
-                    }
-                    onChange={handleSelectAll}
-                  />
-                  <TableCell padding="checkbox">
-                    <Checkbox
-                      checked={selectedpostIds.indexOf(post.id) !== -1}
-                      onChange={(event) => handleSelectOne(event, post.id)}
-                      value="true"
-                    />
-                  </TableCell>
-                  <TableHead>
-              <TableRow>
-                <TableCell padding="checkbox">
-                  
-                </TableCell>
-                <TableCell>
-                  Recent Posts
-                </TableCell>
-                
-              </TableRow>
-            </TableHead>*/
